f16986415cc5d0e5095831a13dded4ad05100796
Suellyn Gomes <130364166+sxellyn@users.noreply.github.com>
2023-10-10 14:31:18 +0000

    first commit

---
+++ things.py
@@ -0,0 +1,22 @@
+#importing things to use:
+import pandas as pd
+import sqlite3
+
+#creating a tibble for using as database:
+data = pd.DataFrame({
+    'ID':[1,2,3,4],
+    'Name': ['Lily', 'Rose', 'Hydrangea', 'Sunflower'] 
+    })
+
+#calling the tibble above:
+data
+
+#acessing the sqlite server and oppening the connection:
+base = sqlite3.connect('mydatabase.db') 
+
+#using the 'to sql' method to save the dataframe to a table:
+data.to_sql('tibble', base, if_exists='replace', index=False)
+
+#closing the connection to the database:
+base.close()
+
